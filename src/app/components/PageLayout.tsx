9ee1fd8967116b20e3d1e665a884b14662b7039f
Khai Nguyen <khai.fordev@gmail.com>
2025-09-10 09:45:09 +0700

    update: layout

--- src/app/components/PageLayout.tsx
+++ src/app/components/PageLayout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useGlobalStore } from '@/store/globalStore';
 
 interface PageLayoutProps {
@@ -15,38 +16,38 @@ export default function PageLayout({
 }: PageLayoutProps) {
   const userInfo = useGlobalStore(state => state.userInfo);
   return (
-    <div className="bg-gray-50 min-h-screen">
-      <div className="mx-auto px-8 md:px-4 max-w-5xl">
-        <div className="flex justify-between items-center mb-2 h-14">
+    <div className="min-h-screen">
+      <div className='bg-amber-500 w-full'>
+        <div className="flex justify-between items-center mx-auto px-4 max-w-5xl h-14 text-white">
           <div className='font-medium text-3xl'>ETApp</div>
           <div className='flex items-center gap-2'>
 
             <div className="flex flex-col justify-center">
               <p className='font-bold'>{userInfo?.firstName} {userInfo?.lastName}</p>
-              <span className="text-gray-500 text-xs">{userInfo?.email}</span>
+              <span className="text-xs">{userInfo?.email}</span>
             </div>
             <div className='border border-slate-200 rounded-full w-10 h-10'>
 
             </div>
           </div>
         </div>
-        <main className=''>
-          <div className="flex justify-between items-center mb-6">
-            <div>
-              <h1 className="font-bold text-gray-600 text-3xl">{title}</h1>
-              <p className="text-gray-500 text-sm">{description}</p>
-            </div>
-            {actions && actions.length > 0 && (
-              <div className="flex items-center gap-3">
-                {actions.map((action, index) => (
-                  <div key={index}>{action}</div>
-                ))}
-              </div>
-            )}
-          </div>
-          {children}
-        </main>
       </div>
+      <main className="mx-auto mt-4 px-4 max-w-5xl max-h-[calc(100%-56px)]">
+        <div className="flex justify-between items-center gap-1 mb-6">
+          <div>
+            <h1 className="font-bold text-gray-600 text-3xl">{title}</h1>
+            <p className="text-gray-500 text-sm">{description}</p>
+          </div>
+          {actions && actions.length > 0 && (
+            <div className="flex items-center gap-3">
+              {actions.map((action, index) => (
+                <div key={index}>{action}</div>
+              ))}
+            </div>
+          )}
+        </div>
+        {children}
+      </main>
     </div>
   );
 }
